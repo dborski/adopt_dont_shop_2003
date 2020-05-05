602e2e314ab255c52d286360349083e039efb594
Derek Borski <derek.borski@gmail.com>
2020-05-05 13:52:40 -0600

    changed vist method to shelters_path instead of /shelters

--- spec/features/user_sees_all_shelters_spec.rb
+++ spec/features/user_sees_all_shelters_spec.rb
@@ -3,14 +3,11 @@ require 'rails_helper'
 describe "user sees all shelters" do
   describe "they visit /shelters" do
     it "displays all shelters" do
-      # shelter1 = Shelter.create!({name: "Shelter1", address: "1 Shelter Ave", city: "1 City", state: "1 State", zip: "1 Zip"})
-      # shelter2 = Shelter.create!({name: "Shelter2", address: "2 Shelter Ave", city: "2 City", state: "2 State", zip: "2 Zip"})
 
-      # shelters = create_list(:shelters, 2)
       shelter1 = create(:shelter)
       shelter2 = create(:shelter)
 
-      visit '/shelters'
+      visit shelters_path
 
       expect(page).to have_content(shelter1.name)
       expect(page).to have_content(shelter2.name)
