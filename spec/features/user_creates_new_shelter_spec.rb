488caa527ed772b54b249f95e29e55d952fc4b07
Derek Borski <derek.borski@gmail.com>
2020-05-05 14:31:18 -0600

    Add test for shelter creation

---
+++ spec/features/user_creates_new_shelter_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "user creates a new shelter" do
+  describe "they link from the shelters index" do
+    describe "they fill in all shelter attributes" do
+      it "creates a new shelter" do
+
+        visit shelters_path
+        click_link "New Shelter"
+
+        expect(current_path).to eq(new_shelter_path)
+
+        fill_in "shelter[name]", with: "New name"
+        fill_in "shelter[address]", with: "New address"
+        fill_in "shelter[city]", with: "New city"
+        fill_in "shelter[state]", with: "New state"
+        fill_in "shelter[zip]", with: "New zip"
+        click_on "Create Shelter"
+
+        expect(page).to have_content("New name")
+        expect(page).to have_content("New address")
+        expect(page).to have_content("New city")
+        expect(page).to have_content("New state")
+        expect(page).to have_content("New zip")
+      end
+    end
+  end
+end
