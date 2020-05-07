a75e0f744e04067a17f722251d6eb1b221391b31
Derek Borski <derek.borski@gmail.com>
2020-05-07 15:11:47 -0600

    Remove story from bottom of test

--- spec/features/user_sees_one_pet_spec.rb
+++ spec/features/user_sees_one_pet_spec.rb
@@ -20,16 +20,3 @@ describe "user sees one pet" do
     end
   end
 end
-
-
-# User Story 9, Pet Show
-#
-# As a visitor
-# When I visit '/pets/:id'
-# Then I see the pet with that id including the pet's:
-# - image
-# - name
-# - description
-# - approximate age
-# - sex
-# - adoptable/pending adoption status
