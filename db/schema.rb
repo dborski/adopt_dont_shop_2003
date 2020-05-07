1ba72487ee5fd0d32f4b99b8655c7b5bee5ce3d1
Derek Borski <derek.borski@gmail.com>
2020-05-07 14:22:07 -0600

    Add description column to pets table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200507034433) do
+ActiveRecord::Schema.define(version: 20200507201820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20200507034433) do
     t.string "img_url"
     t.bigint "shelter_id"
     t.string "adoption_status", default: "adoptable"
+    t.string "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
