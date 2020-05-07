b308b6444dd2609087bc395536245e0131663ef1
Derek Borski <derek.borski@gmail.com>
2020-05-06 23:57:11 -0600

    Add spec for adoptable_pets method and add method to pets model

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -7,4 +7,16 @@ describe Pet, type: :model do
   describe "relationships" do
     it { should belong_to :shelter }
   end
+  describe "methods" do
+    it ".adoptable_pets" do
+      shelter = create(:shelter)
+      pet1 = create(:pet, shelter_id: "#{shelter.id}", adoption_status: "pending")
+      pet2 = create(:pet, shelter_id: "#{shelter.id}")
+      pet3 = create(:pet, shelter_id: "#{shelter.id}")
+
+      pets = Pet.all
+
+      expect(pets.adoptable_pets).to eq([pet2, pet3])
+    end
+  end
 end
