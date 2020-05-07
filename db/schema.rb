0f81731bdb4c974480b155c2014d8564dd6545be
Derek Borski <derek.borski@gmail.com>
2020-05-06 21:13:59 -0600

    Pets migration to add adoptable column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200506215058) do
+ActiveRecord::Schema.define(version: 20200507030704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20200506215058) do
     t.string "sex"
     t.string "img_url"
     t.bigint "shelter_id"
+    t.string "adoptable"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
