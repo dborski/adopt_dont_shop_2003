11f07cfa619d93bd9126e7eb09a379c15a028608
Derek Borski <derek.borski@gmail.com>
2020-05-07 21:31:24 -0600

    Add validation that sex must be male or female

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
   validates_presence_of :name
+  validates :sex, inclusion: { in: %w(male female),
+    message: "%{value} is not a valid sex" }
 
   def self.adoptable_pets
     select { |pet| pet.adoption_status == "adoptable" }
