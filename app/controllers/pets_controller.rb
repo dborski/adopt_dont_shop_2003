74311fc9c07b2dbad40a0fc80077368eb4822772
Derek Borski <derek.borski@gmail.com>
2020-05-07 21:30:57 -0600

    Add shelter_pets routes and pets new and create routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,8 +4,26 @@ class PetsController < ApplicationController
     @pets = Pet.all
   end
 
+  def new
+    @shelter_id = params[:shelter_id]
+  end
+
   def show
     @pet = Pet.find(params[:id])
   end
 
+  def create
+    shelter = Shelter.find(params[:shelter_id])
+    pet = shelter.pets.create(pet_params)
+
+    pet.save
+
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
+
+  private
+
+  def pet_params
+    params.permit(:name, :img_url, :age, :sex, :description)
+  end
 end
