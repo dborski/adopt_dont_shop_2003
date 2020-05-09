626962b610ba3f05312a89a2d91b0fccb28e72fb
Derek Borski <derek.borski@gmail.com>
2020-05-09 09:59:45 -0600

    Add spec for edit links in pets and shelter_pets. Add links in views

--- spec/features/views_spec.rb
+++ spec/features/views_spec.rb
@@ -27,3 +27,38 @@ describe "user can delete each shelter from shelters index" do
     end
   end
 end
+
+describe "user can edit each shelter from pets index" do
+  describe "they link from the pets index" do
+    it "edits the shelter" do
+
+      pet = create(:pet)
+        
+      visit "/pets/"
+
+      expect(page).to have_link("Edit Pet", :href=>"/pets/#{pet.id}/edit")
+    end
+  end
+end
+
+describe "user can edit each shelter from shelters/pets index" do
+  describe "they link from the shelter/pets index" do
+    it "edits the shelter" do
+
+      shelter = create(:shelter)
+      pet = create(:pet, shelter_id: "#{shelter.id}")
+        
+      visit "shelters/#{shelter.id}/pets/"
+
+      expect(page).to have_link("Edit Pet", :href=>"/pets/#{pet.id}/edit")
+    end
+  end
+end
+
+# User Story 15, Pet Update From Pets Index Page
+
+# As a visitor
+# When I visit the pets index page or a shelter pets index page
+# Next to every pet, I see a link to edit that pet's info
+# When I click the link
+# I should be taken to that pets edit page where I can update its information just like in User Story 11
