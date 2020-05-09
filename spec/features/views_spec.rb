0962e77e33a7e67270de5cdb53bd5801c4d8483d
Derek Borski <derek.borski@gmail.com>
2020-05-09 11:27:26 -0600

    Add shelters index link to top of page

--- spec/features/views_spec.rb
+++ spec/features/views_spec.rb
@@ -136,10 +136,3 @@ describe "user can click on pet name to take them to pets/id page" do
     end
   end
 end
-
-
-# User Story 19, Pet Index Link
-
-# As a visitor
-# When I visit any page on the site
-# Then I see a link at the top of the page that takes me to the Pet Index
